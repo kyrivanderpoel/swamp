35f0e5d269bff032b6fd0cbc5eeab7982b25bbf9
kbougy <kyriayxvanderpoel@gmail.com>
2018-02-25 23:00:23 -0600

    Implement deactivate feature

--- swamp.go
+++ swamp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"log"
 	"os"
 	"os/user"
@@ -9,7 +10,7 @@ import (
 	"path/filepath"
 )
 
-func main() {
+func activate() {
 	u, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -35,9 +36,70 @@ func main() {
 
 	for _, f := range files[1:] {
 		newPath := path.Join(u.HomeDir, path.Base(f))
+		fmt.Printf("Activated %s...\n", newPath)
 		err := os.Symlink(f, newPath)
-		if err != nil {
-			log.Fatal()
+
+		// Continue running if file already existed
+		fileExistsErrorMsg := fmt.Sprintf("symlink %s %s: file exists", f, newPath)
+		if err != nil && err.Error() != fileExistsErrorMsg {
+			fmt.Println(err.Error())
+			log.Fatal(err)
 		}
+	}
+}
+
+func deactivate() {
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	swampPath := path.Join(u.HomeDir, ".swamp")
+
+	// Remove all the symlinks
+	files := []string{}
+	filepath.Walk(swampPath, func(p string, f os.FileInfo, err error) error {
+		files = append(files, p)
+		return nil
+	})
+
+	for _, f := range files[1:] {
+		symPath := path.Join(u.HomeDir, path.Base(f))
+		fmt.Printf("Deactivating %s...\n", symPath)
+		err = os.Remove(symPath)
+		if err != nil {
+			fmt.Printf("File did not exist: %s\n", symPath)
+		}
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "swamp"
+	app.Usage = "organize and share your configuration files with ease"
+	app.Commands = []cli.Command{
+		{
+			Name:    "activate",
+			Aliases: []string{"a"},
+			Usage:   "activate your swamp",
+			Action: func(c *cli.Context) error {
+				activate()
+				return nil
+			},
+		},
+		{
+			Name:    "deactivate",
+			Aliases: []string{"a"},
+			Usage:   "deactivate your swamp",
+			Action: func(c *cli.Context) error {
+				deactivate()
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
